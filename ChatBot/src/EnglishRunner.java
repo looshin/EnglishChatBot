03b21c4a583cfdd73d40637a106bfe6a7a81a5db
looshin <uleek500@gmail.com>
2022-07-29 10:22:30 -0700

    Commented version of code

--- ChatBot/src/EnglishRunner.java
+++ ChatBot/src/EnglishRunner.java
@@ -10,17 +10,21 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class EnglishRunner {
-
+	/*
+	 * 	The main method displays everything onto the console
+	 *  @param args console input
+	 */
 	public static void main(String[] args) throws FileNotFoundException, IOException {
-		int DifficultyLevel = 1;
-		English.difficultyLevel(DifficultyLevel);
-		ArrayList<String> categories = new ArrayList<String>();
+		int DifficultyLevel = 1; ///setting difficulty level
+		English.difficultyLevel(DifficultyLevel); 
+		ArrayList<String> categories = new ArrayList<String>(); //adding categories
 		categories.add("greetings");
 		categories.add("endconvo");
 		
+		//Creating a new english chat bot
 		English spanishBot = new English(categories);
 		spanishBot.runBot();
-		English.createAnswers();
+		English.createAnswers(); //Creating answers
 		
 		
 	}
